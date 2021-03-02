a37371e3fecec64033b329f256a82086228b4cc5
satrancci <thescarface1993@gmail.com>
2021-03-02 15:08:16 -0600

    added commands to build/pull Docker images that we will be working with

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@ python3 -m pip install --upgrade pip
 python3 -m pip install --user virtualenv
 python3 -m venv env
 source env/bin/activate && python3 -m pip install -r requirements.txt
+
+docker build -t selenium .
+docker pull bubuntux/nordvpn
