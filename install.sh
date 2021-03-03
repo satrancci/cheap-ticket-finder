2bcf345cb06743e1460383c9a191538b31d35095
satrancci <thescarface1993@gmail.com>
2021-03-02 22:02:16 -0600

    add instructions for .env

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@ python3 -m pip install --upgrade pip
 python3 -m pip install --user virtualenv
 python3 -m venv env
 source env/bin/activate && python3 -m pip install -r requirements.txt
+touch .env
+# Then open .env and manually store:
+# NORDVPN_USERNAME="<YOUR_USERNAME>"
+# NORDVPN_PASSWORD="<YOUR_PASSWORD>"
 
 docker build -t selenium .
 docker pull bubuntux/nordvpn
+
