23d427b5629f0ca0793c792ae2438c65c3313fa1
satrancci <thescarface1993@gmail.com>
2021-04-06 01:54:08 -0500

    minor update

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ python3 -m pip install --upgrade pip
 python3 -m pip install --user virtualenv
 python3 -m venv env
 source env/bin/activate && python3 -m pip install -r requirements.txt
-mkdir -p crawled_data/expedia
+mkdir -p crawled_data
 touch .env
 # Then open .env and manually store:
 # NORDVPN_USERNAME="<YOUR_USERNAME>"
